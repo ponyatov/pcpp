2c9a3f3b3156bcfa9bb0608fb0ae643c5044cd8e
Dmitry Ponyatov <dponyatov@gmail.com>
2025-11-27 11:29:22 +0400

    .

--- mk/ref.mk
+++ mk/ref.mk
@@ -1,3 +1,3 @@
 RF += ref/PcapPlusPlus/README.md
 ref/PcapPlusPlus/README.md:
-    $(GITREF) git@github.com:ponyatov/PcapPlusPlus.git $(dir $@)
+	$(GITREF) git@github.com:ponyatov/PcapPlusPlus.git $(dir $@)
