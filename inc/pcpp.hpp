84c1a91a5cad471fc93d4e68401a2efbc081abcd
Dmitry Ponyatov <dponyatov@gmail.com>
2025-12-01 18:09:39 +0400

    .

--- inc/pcpp.hpp
+++ inc/pcpp.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
-#define MBUF_POOL_SZ (8192 - 1)
+// #define MBUF_POOL_SZ (8192 - 1)
 // #define MBUF_POOL_SZ (0x10000 - 1)
+#define MBUF_POOL_SIZE (16 * 1024 - 1)
 
 #include <DpdkDevice.h>
 #include <DpdkDeviceList.h>
@@ -9,8 +10,42 @@
 
 #include <cassert>
 #include <iostream>
+#include <thread>
 
 extern int main(int argc, char* argv[]);
 extern void arg(int argc, char* argv);
 
-extern void init(int argc, char* argv[]);
+class Dev {
+    static pcpp::CoreMask coreMaskToUse;
+    static pcpp::DpdkDevice* dev;
+
+   public:
+    static void init(int argc, char* argv[]);
+    static std::vector<pcpp::DpdkWorkerThread*> workers;
+    static void on_close();
+    static void onApplicationInterrupted(void*);
+};
+
+class Send : public pcpp::DpdkWorkerThread {
+    pcpp::DpdkDevice* dev;
+    bool stop;
+
+    pcpp::Packet packet;
+    uint count;
+
+   public:
+    Send(pcpp::DpdkDevice* dev) : dev(dev) {}
+};
+
+class Stat : public pcpp::DpdkWorkerThread {
+    pcpp::DpdkDevice* dev;
+    bool _stop;
+    uint32_t _coreId;
+    pcpp::DpdkDevice::DpdkDeviceStats stats;
+
+   public:
+    Stat(pcpp::DpdkDevice* dev) : dev(dev) { _stop = false; }
+    void stop() { _stop = true; }
+    uint32_t getCoreId() const { return dev->getCurrentCoreId(); }
+    bool run(uint32_t coreId);
+};
--- src/pcpp.cpp
+++ src/pcpp.cpp
@@ -5,7 +5,7 @@ extern int main(int argc, char* argv[]) {  //
     for (int i = 1; i < argc; i++) {  //
         arg(i, argv[i]);
     }
-    init(argc, argv);
+    Dev::init(argc, argv);
     return 0;
 }
 
@@ -13,7 +13,63 @@ extern void arg(int argc, char* argv) {  //
     std::clog << "arg[" << argc << "] = <" << argv << "]\n";
 }
 
-extern void init(int argc, char* argv[]) {  //
-    pcpp::CoreMask coreMaskToUse(0x1);
-    assert(pcpp::DpdkDeviceList::initDpdk(coreMaskToUse, MBUF_POOL_SZ));
+pcpp::DpdkDevice* Dev::dev = nullptr;
+pcpp::CoreMask Dev::coreMaskToUse = pcpp::getCoreMaskForAllMachineCores();
+
+void Dev::init(int argc, char* argv[]) {  //
+    Dev::on_close();
+    assert(pcpp::DpdkDeviceList::initDpdk(coreMaskToUse, MBUF_POOL_SIZE));
+    assert(dev = pcpp::DpdkDeviceList::getInstance().getDeviceByPort(0));
+    std::clog << "Dev: " << dev->getDeviceName()           //
+              << " id:" << dev->getDeviceId()              //
+              << " name:" << dev->getPMDName()             //
+              << " tx:" << dev->getTotalNumOfTxQueues()    //
+              << " rx:" << dev->getTotalNumOfRxQueues()    //
+              << " mac:" << dev->getMacAddress()           //
+              << " pci:" << dev->getPciAddress()           //
+              << " mbuf:" << dev->getAmountOfMbufsInUse()  //
+              << '/' << dev->getAmountOfFreeMbufs()        //
+              << "\n";
+    assert(dev->openMultiQueues(1, 1));
+    Dev::workers.push_back(new Stat(dev));
+    pcpp::DpdkDeviceList::getInstance().startDpdkWorkerThreads(0b10,
+                                                               Dev::workers);
+}
+
+void Dev::on_close() {
+    pcpp::ApplicationEventHandler::getInstance().onApplicationInterrupted(
+        Dev::onApplicationInterrupted, NULL);
+}
+
+void Dev::onApplicationInterrupted(void*) {  //
+    pcpp::DpdkDeviceList::getInstance().stopDpdkWorkerThreads();
+    dev->close();
+    fprintf(stderr, "\nShutting down...\n");
+    // std::clog << "interrupted:" << dev->getDeviceName() << "\n";
+}
+
+std::vector<pcpp::DpdkWorkerThread*> Dev::workers;
+
+bool Stat::run(uint32_t coreId) {  //
+    _coreId = coreId;
+    for (uint n = 0; !_stop; n++) {
+        dev->getStatistics(stats);
+
+        std::clog << "stat:" << n                                      //
+                  << " core:" << getCoreId()                           //
+                  << " mac:" << dev->getMacAddress()                   //
+                  << " mbuf:" << dev->getAmountOfMbufsInUse()          //
+                  << '/' << dev->getAmountOfFreeMbufs()                //
+                  << " packets:" << stats.aggregatedRxStats.packets    //
+                  << '/' << stats.aggregatedTxStats.packets            //
+                  << " pps:" << stats.aggregatedRxStats.packetsPerSec  //
+                  << '/' << stats.aggregatedTxStats.packetsPerSec      //
+                  << " bytes:" << stats.aggregatedRxStats.bytes        //
+                  << '/' << stats.aggregatedTxStats.bytes              //
+                  << " bps:" << stats.aggregatedRxStats.bytesPerSec    //
+                  << '/' << stats.aggregatedTxStats.bytesPerSec        //
+                  << "\n";
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    return true;
 }
