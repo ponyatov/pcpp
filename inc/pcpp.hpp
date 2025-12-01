28ea6a73818f7e2ec066a732e5b31f1286c7caba
Dmitry Ponyatov <dponyatov@gmail.com>
2025-12-01 16:52:40 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- inc/pcpp.hpp
+++ inc/pcpp.hpp
@@ -3,3 +3,8 @@
 #include <DpdkDevice.h>
 #include <DpdkDeviceList.h>
 #include <SystemUtils.h>
+
+#include <iostream>
+
+extern int main(int argc, char* argv[]);
+extern void arg(int argc, char* argv);
