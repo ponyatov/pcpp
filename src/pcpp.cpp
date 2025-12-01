e18b10ac0992c93f92b622feb0e83df9b41fdc1c
Dmitry Ponyatov <dponyatov@gmail.com>
2025-12-01 16:53:33 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/pcpp.cpp
+++ src/pcpp.cpp
@@ -1,3 +1,13 @@
 #include "pcpp.hpp"
 
-int main() {}
+extern int main(int argc, char* argv[]) {  //
+    arg(0, argv[0]);
+    for (int i = 1; i < argc; i++) {  //
+        arg(i, argv[i]);
+    }
+    return 0;
+}
+
+extern void arg(int argc, char* argv) {  //
+    std::clog << "arg[" << argc << "] = <" << argv << "]\n";
+}
