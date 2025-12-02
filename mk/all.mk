039e5647c46c9b911e59c1ea6a8a8ca4a060633b
Dmitry Ponyatov <dponyatov@gmail.com>
2025-12-02 17:47:25 +0400

    .

--- mk/all.mk
+++ mk/all.mk
@@ -3,7 +3,7 @@ PCI=0000:01:00.0
 .PHONY: all run watch
 all: bin/$(APP)
 run: bin/$(APP)
-	$^ -a $(PCI)
+	$^ $(PCI)
 watch: bin/$(APP)
 # 	@$^ ; while [ true ]; do $^ ; done
-	@$^ ; while [ $$? -eq 1 ]; do $^ -a $(PCI) ; done
+	@$^ ; while [ $$? -eq 1 ]; do $^ $(PCI) ; done
